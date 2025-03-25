31988273fbd65f1750f20f855e91f1bcc59372fd
caiocroccia22 <caiocroccia22@gmail.com>
2025-03-24 21:27:40 -0300

    feat: implement getters and setters / use polymorphism concept

--- funcionario.js
+++ funcionario.js
@@ -12,15 +12,49 @@ function Pessoa(nome){
 
 function Funcionario(nome, cargo, salario){
     this.cargo = cargo;
-    this.salario = salario;
+    let _salario = salario;
+
+    this.getSalario = function(){
+        return _salario;
+    }
+
+    this.setSalario = function(valor){
+        if (typeof valor === 'number'){
+            _salario = valor; 
+        }
+    }
+
+    this.aumento = function(){
+        const novoSalario = _salario * 1.1;
+        // console.log(novoSalario);
+        _salario = novoSalario;
+    }
 
     // Herança de atributos da classe Pessoa
     Pessoa.call(this, nome);
 }
 
+function Estagiario(nome){
+    Funcionario.call(this, nome, "Dev front-end Júnior", 2000)
+
+    this.aumento = function(){
+        const novoSalario = this.getSalario() * 1.07;
+        this.setSalario(novoSalario);
+    }
+}
+
 
 const funcionario1 = new Funcionario("Maria", "Dev front-end", 5000);
+const funcionario2 = new Estagiario("Pedro");
 const pessoa = new Pessoa('Caio Crocccia')
 funcionario1.dizOi();
 pessoa.dizCargo(); // undefined
 
+// funcionario1.salario = 1000
+// console.log(funcionario1)
+
+funcionario1.aumento();
+console.log(funcionario1.getSalario())
+
+funcionario2.aumento();
+console.log(funcionario2.getSalario())
